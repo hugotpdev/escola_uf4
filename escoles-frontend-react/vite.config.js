f594f35f9d153bbff0d85010e42b16eaf599827c
hugotpdev <htablaprieto@gmail.com>
2025-05-18 22:33:19 +0200

    React final 3

--- escoles-frontend-react/vite.config.js
+++ escoles-frontend-react/vite.config.js
@@ -5,5 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+    allowedHosts: ['escolauf4-production.up.railway.app']
   }
 });
